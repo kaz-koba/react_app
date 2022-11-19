676a00c1315269f1396cade451cca7b8c409836a
shaream <sprinter0208@outlook.jp>
2022-11-19 18:48:05 +0900

    ta

--- src/bad/bad1.js
+++ src/bad/bad1.js
@@ -1,8 +1,10 @@
-app.post("/records", (request, response) => {
-  const data = request.body;
-  const query = `SELECT * FROM health_records WHERE id = (${data.id})`;
-  connection.query(query, (err, rows) => {
-    if(err) throw err;
-    response.json({data:rows});
+export const click = () => {
+  app.post("/records", (request, response) => {
+    const data = request.body;
+    const query = `SELECT * FROM health_records WHERE id = (${data.id})`;
+    connection.query(query, (err, rows) => {
+      if (err) throw err;
+      response.json({ data: rows });
+    });
   });
-});
+};
