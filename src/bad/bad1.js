be335950bd679c2e6eda0172e275ebbbb3e57fa8
shaream <sprinter0208@outlook.jp>
2022-11-19 17:59:56 +0900

    bad

---
+++ src/bad/bad1.js
@@ -0,0 +1,20 @@
+// app.js
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'ホスト名',
+  user: 'ユーザー名',
+  password: 'パスワード',
+  database: 'データベース名'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+ 
+connection.query('SELECT * FROM user', (err,rows) => {
+  if(err) throw err;
+ 
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+ 
