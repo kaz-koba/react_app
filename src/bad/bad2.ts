a618b1928cf6bf1275c70718c12ccdb13fdb83e0
shaream <sprinter0208@outlook.jp>
2022-11-21 21:56:43 +0900

    bad2

--- src/bad/bad2.ts
+++ src/bad/bad2.ts
@@ -1,15 +1,13 @@
 import Express from "express";
-import fetch from "node-fetch";
 
-type ArticleResponse = { article: { title: string; body_html: string } };
+type ArticleResponse = { aaa: { title: string; body_html: string } };
 
 export async function getCliGuide(req: Express.Request, res: Express.Response) {
   const slug = req.params.slug;
   try {
-    const response = await fetch(`https://zenn.dev/api/articles/${slug}`, "a");
-    const { article }: ArticleResponse = await response.json();
-    const { body_html, title } = article;
-    res.setHeader("Cache-Control", "public, max-age=7200"); // cache on browser
+    const response = await fetch(`https://localhost:9000${slug}`);
+    const { aaa }: ArticleResponse = await response.json();
+    const { body_html, title } = aaa;
     res.json({ bodyHtml: body_html, title });
   } catch (err) {
     console.log(err);
